f0e8d22304631880fc1324da4252f6c68129bb2a
atlasboris <atlasboris@gmail.com>
2021-03-13 18:15:32 +0200

    material design select (without form) test

--- src/app/mat-select-tests/mat-select-tests.component.ts
+++ src/app/mat-select-tests/mat-select-tests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mat-select-tests',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mat-select-tests.component.scss']
 })
 export class MatSelectTestsComponent implements OnInit {
+  branchesList: iBranch[] = [
+    {
+      branchId: 111,
+      branchName: 'Yavne'
+    },
+    {
+      branchId: 2222,
+      branchName: 'Tel Aviv'
+    },
+    {
+      branchId: 333,
+      branchName: 'Rishon'
+    },
+  ];
 
-  constructor() { }
+  selectedBranches = new FormControl();
 
   ngOnInit(): void {
   }
 
+  onClick(){
+    console.log("selection: ", this.selectedBranches)
+  }
+
+}
+
+export interface iBranch {
+  branchId: number,
+  branchName: string
 }
